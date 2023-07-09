4353e709b778d594b6e6fdfa3ef0f392fd17addd
kokuri46keita <kokuri46keita@gmail.com>
2023-07-09 22:05:40 +0900

    window

--- game.h
+++ game.h
@@ -1,12 +1,14 @@
 #pragma once
 #include"DxLib.h"
 
+const int windowx = 800;
+const int windowy = 800;
 
-const int width = 10;
+const int width = 15;
 const int vertical = 8;
-const int blockscale = 50;
 const int startx = 50;
 const int starty = 300;
+const int blockscale = (windowx - startx ) / width - (startx / width);
 const int erasenum = 4;
 extern int blocknum;
 extern bool gameclearflag;
